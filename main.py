33171c1d511913f0ac300caab71ffb3fd96dfd6e
rahrahr <pines_Yuan@outlook.com>
2021-06-22 16:29:36 +0800

    Commit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
 from gui import Ui
 from PyQt5 import QtWidgets, uic
+import os
 
 if __name__ == '__main__':
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    sys.argv += ['--style', 'fusion']
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
     window.show()
--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
 import QuantLib as ql
 import pandas as pd
-from WindPy import *
+# from WindPy import *
 import time
 import numpy as np
 
-w.start()
-today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+# w.start()
+# today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
 
 
 def get_basic_info(code: str) -> dict:
     # 获取左上部分信息
 
-    info = w.wsd(code, "exch_city,exchange_cn,sec_type,settlementmethod",
-                 "ED0D", today, "PriceAdj=CP")
-    info = np.ravel(info.Data)
+    # info = w.wsd(code, "exch_city,exchange_cn,sec_type,settlementmethod",
+    #              "ED0D", today, "PriceAdj=CP")
+    # info = np.ravel(info.Data)
     basic_info = {'location': info[0],
                   'platform': info[1],
                   'quote_convention': '',
@@ -27,16 +27,16 @@ def get_quote(code: str) -> pd.DataFrame:
     quote_df = pd.DataFrame(None, index=['code', 'clean', 'full', 'yield'],
                             columns=['IB', 'SH', 'SZ'])
 
-    quote_ib = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
-                     "returnType=1;TradingCalendar=NIB")
-    quote_df['IB'] = pd.Series(np.ravel(quote_ib.Data), index=quote_df.index)
+    # quote_ib = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
+    #                  "returnType=1;TradingCalendar=NIB")
+    # quote_df['IB'] = pd.Series(np.ravel(quote_ib.Data), index=quote_df.index)
 
-    quote_sh = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
-                     "returnType=1;TradingCalendar=SHFE")
-    quote_df['SH'] = pd.Series(np.ravel(quote_sh.Data), index=quote_df.index)
-    quote_sz = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
-                     "returnType=1;TradingCalendar=SZSE")
-    quote_df['SZ'] = pd.Series(np.ravel(quote_sz.Data), index=quote_df.index)
+    # quote_sh = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
+    #                  "returnType=1;TradingCalendar=SHFE")
+    # quote_df['SH'] = pd.Series(np.ravel(quote_sh.Data), index=quote_df.index)
+    # quote_sz = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
+    #                  "returnType=1;TradingCalendar=SZSE")
+    # quote_df['SZ'] = pd.Series(np.ravel(quote_sz.Data), index=quote_df.index)
 
     return quote_df
 
